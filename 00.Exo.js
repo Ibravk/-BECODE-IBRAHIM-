39cdd167cd7bad6bf2e552a947c9dec43d5fce77
Ibrahim <poyrazibrahim270@gmail.com>
2024-09-14 12:45:19 +0200

    Exercices Algo serie 1

---
+++ 00.Exo.js
@@ -0,0 +1,32 @@
+// 00.Exo ===========
+
+function makesTen(a, b) {
+    if (a, b === 10 || a + b === 10) {
+       return true; 
+    } else 
+       return false; 
+    
+}
+
+// 01.Exo ===========
+
+function calcAge(day) {
+   const myAgeindays = 365; 
+   return myAgeindays * day; 
+}
+calcAge(); 
+
+
+// 02.Exo ===========
+
+// Créer une fonction qui prend un nombre comme argument. Additionnez tous les nombres de 1 au nombre que vous avez passé à la fonction. Par exemple, si l'entrée est 4 alors votre fonction devrait retourner 10 parce que 1 + 2 + 3 + 4 = 10.
+
+
+function addUp(num) {
+   const add = num.
+   return add 
+   
+}
+
+
+console.log(addUp(23)); 
