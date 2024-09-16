c4f644044f9cee08cc0e5dfdd6cfa0ac260fbaac
Ibrahim <poyrazibrahim270@gmail.com>
2024-09-16 16:35:43 +0200

    final modify

--- 00.Exo.js
+++ 00.Exo.js
@@ -68,3 +68,60 @@ console.log(distributor())
 
 
 // 06.Exo ==========
+
+
+
+function animals(chickens, cows, pigs) {
+
+      let chickensLegs = 2; 
+      let cowsLegs = 4; 
+      let pigsLegs = 4; 
+   
+   let legAnimal = (chickens * chickensLegs ) + 
+                   (cows * cowsLegs) + 
+                   (pigs * pigsLegs)
+
+   return legAnimal; 
+}
+
+console.log(animals(1, 2, 3))
+
+
+// 07.Exo ==========
+
+
+function profitableGamble(probability, prize, pay){
+
+   if ( (probability * prize) > pay ) {  
+
+      return true; 
+
+   } else 
+
+      return false
+}
+
+profitableGamble(0.2, 50, 9) //  retournera true car le gain attendu est de 0.2 * 50 = 10, ce qui est supérieur au coût de 9.
+
+
+// 08.Exo ==========
+
+function imageSecond(image, second) {
+
+   let nexImage = ((image * 60 ) * second); 
+   return nexImage
+   
+}
+console.log(imageSecond(1, 1)); 
+
+
+
+// 09.Exo ==========
+
+function calculateFuel(distance) {
+   let fuelNeed = distance * 10;
+   return Math.max(fuelNeed, 100); //La fonction Math.max() est utilisée pour retourner le plus grand nombre entre fuelNeed et 100, 
+}
+
+console.log(calculateFuel(3)); 
+
